d75fb796d08813785361cfa6ee6eb8fc1e4b3723
Muntajir Hussain Gazi <muntajirwork11@gmail.com>
2024-11-26 14:15:00 +0530

    Add plant feature & image picker added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { theme } from '@/theme';
+import { usePlantStore } from '@/store/plantsStore';
+import { PlantCard } from '@/components/PlantCard';
+import { PlantlyButton } from '@/components/PlantlyButton';
+import { useRouter } from 'expo-router';
 
 export default function App() {
+  const router = useRouter();
+  const plants = usePlantStore(state => state.plants);
+
   return (
-    <View style={styles.container}>
-      
-      <Text>Start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList 
+    style = {styles.container}
+    contentContainerStyle= {styles.contentContainer}
+      data={plants}
+      renderItem={({ item }) => (
+       <PlantCard plant={item} />
+      )}
+
+      ListEmptyComponent={
+        <PlantlyButton
+        title='Add your first plant'
+        onPress={() => router.navigate('/new')}
+        />
+      }
+
+    />
   );
 }
 
@@ -16,7 +34,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  contentContainer: {
+    padding: 12,
+  }
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from "expo-router";
 export default function Layout() {
   return (
   <Stack>
-    <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+    <Stack.Screen name="(tabs)" options={{headerShown: false, animation : "fade"}} />
     <Stack.Screen name="onboarding" options={{headerShown: false, animation : "fade"}} />
+    <Stack.Screen name="new" options={{presentation: "modal", title : "New Plant"}} />
+    
   </Stack>)
 }
